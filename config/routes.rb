485ebfaabdcf1a2d48e852f051c1628a58fe7edc
mpakus <mrak69@gmail.com>
2017-08-17 00:20:40 +0700

    main page design;

--- config/routes.rb
+++ config/routes.rb
@@ -8,18 +8,17 @@ Rails.application.routes.draw do
   get '@:user' => 'posts#user', as: :user
   resources :settings, controller: 'settings/settings', only: %i[index create]
 
-  # /home-page
   get ':blog' => 'posts/blogs#index', as: :blog, defaults: { format: 'html' }
-  # /home-page/100-title.html
+  #
   get ':blog/:id(:format)' => 'posts/posts#show', as: :show_post
-  namespace :futuro do
-    resources :posts, except: %i[new show] do
-      resources :blocks, only: [:create], controller: 'posts/blocks'
-      resources :publishes, only: [:create], controller: 'posts/publishes'
-      resources :texts, only: [:update], controller: 'posts/blocks/texts'
-      resources :albums, only: %i[create destroy], controller: 'posts/blocks/albums', shallow: true do
-        resources :photos, only: %i[create destroy], controller: 'posts/blocks/albums/photos'
-      end
-    end
-  end
+  # namespace :futuro do
+  #   resources :posts, except: %i[new show] do
+  #     resources :blocks, only: [:create], controller: 'posts/blocks'
+  #     resources :publishes, only: [:create], controller: 'posts/publishes'
+  #     resources :texts, only: [:update], controller: 'posts/blocks/texts'
+  #     resources :albums, only: %i[create destroy], controller: 'posts/blocks/albums', shallow: true do
+  #       resources :photos, only: %i[create destroy], controller: 'posts/blocks/albums/photos'
+  #     end
+  #   end
+  # end
 end
