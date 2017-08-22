69490508f4da4557bdeddef1f69811f32244060b
mpakus <mrak69@gmail.com>
2017-08-22 11:31:11 +0700

    post/block factory fixed;

--- app/models/post/block.rb
+++ app/models/post/block.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 class Post::Block < ActiveRecord::Base
-  belongs_to :post, required: true
-  belongs_to :blockable, polymorphic: true
-  acts_as_list scope: :post
-
   KINDS = %w[text album].freeze
+
+  belongs_to :post, required: true
+  belongs_to :blockable, polymorphic: true, optional: true
+  acts_as_list scope: :post
 end
 
 # == Schema Information
